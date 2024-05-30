40cd41cda34ed8b984f131ff2211d13eefa71681
Vicente Gárate Piñeda <garate.vicente@gmail.com>
2024-05-30 17:47:32 -0400

    proyecto base crud productos, proveedores y usuarios, con las dependencias basicas para el proyecto

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { ProvidersModule } from './providers/providers.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ProductsModule,
+    ProvidersModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
